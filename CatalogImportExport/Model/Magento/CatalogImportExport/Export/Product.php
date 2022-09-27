e1a419fb9fab853f988a71649f9a2d720d324c55
prasanthki95 <prasanth.kumar@i95dev.com>
2022-09-27 19:42:15 +0530

    New files added

---
+++ CatalogImportExport/Model/Magento/CatalogImportExport/Export/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace I95dev\CatalogImportExport\Model\Magento\CatalogImportExport\Export;
+
+
+class Product extends \Magento\CatalogImportExport\Model\Export\Product
+{
+        public function __construct()
+        {
+            echo "Sucessfully Overided"; die();
+        }
+}
---
+++ CatalogImportExport/registration.php
@@ -0,0 +1,4 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'I95dev_CatalogImportExport', __DIR__);
